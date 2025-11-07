1c65c9078086df4c1b29704bfea36eed3cc62956
Muhamad Sulaiman <mdsulaiman010@gmail.com>
2025-11-08 01:25:20 +0800

    Added function to connect to mongodb

--- db_functions.py
+++ db_functions.py
@@ -1,5 +1,13 @@
 import hashlib
 import sqlite3
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+import gridfs
+
+####################################
+#####     SQLite Functions     #####
+####################################
 
 def connect_db():
     # Open and connect to database to run SQL
@@ -42,3 +50,28 @@ def authenticate(username, password):
     conn.close()
 
     return True if user else False
+
+
+#####################################
+#####     MongoDB Functions     #####
+#####################################
+
+def connect_mongodb():
+    # Load in relevant environment variables
+    load_dotenv()
+    MONGO_ID = os.environ['MONGODB_ID']
+    MONGO_PW = os.environ['MONGODB_PW']
+    MONGO_URI = os.environ['MONGODB_LOOKBOOK_CLUSTER']
+    MONGO_URI = MONGO_URI.replace('<user_id>', MONGO_ID).replace('<user_pw>', MONGO_PW)
+
+    try:
+        # Define MongoDB client
+        client = MongoClient(MONGO_URI)
+        db = client['clothes']
+        fs = gridfs.GridFS(db, collection="images")
+        
+        return client, db, fs
+
+    except Exception as e:
+        print(f'Error initializing DB. Error:\n{e}')
+        return None, None, None
